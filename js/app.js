bd3593c8b8458ddf1a03b203b020172b5912cf8d
Sazzad-Hosen-Mahim <sajjadhosenmahim@gmail.com>
2022-09-05 18:09:20 +0600

    error handler added

--- js/app.js
+++ js/app.js
@@ -1,13 +1,21 @@
 const category = async() =>{
-    const url = 'https://openapi.programming-hero.com/api/news/categories';
+    
+
+    try{
+        const url = 'https://openapi.programming-hero.com/api/news/categories';
     const res = await fetch(url);
     const data = await res.json();
-    // console.log(data.data.news_category);
+    // console.log(data);
     newsCategory(data.data.news_category)
+    }
+    catch(error){
+        console.log(error);
+    }
     
 }
 
 const newsCategory = cat =>{
+    // console.log(cat);
     const categoriesContainer = document.getElementById('category-container');
     cat.forEach(category => {
         // console.log(category.category_name);
@@ -20,7 +28,7 @@ const newsCategory = cat =>{
         
       
     })
-    loadNews(category.category_id);
+    
 
     
     
@@ -31,8 +39,8 @@ const newsCategory = cat =>{
 
 
 
-const categoryN = category();
-// console.log(categoryN);
+category();
+
 
 
 
@@ -42,33 +50,51 @@ const categoryN = category();
 
 const loadNews = async(news) =>{
     // console.log(news);
+   try{
     const url = `https://openapi.programming-hero.com/api/news/category/0${news}`
     const res = await fetch(url);
     const data = await res.json();
     displayNews(data.data);
+   }
+   catch(error){
+    console.log(error);
+   }
 }
 
 const displayNews = (data) => {
     // console.log(data);
-    const newsContainer = document.getElementById('news-container')
+    // data length work
+
+    const newsCounter = document.getElementById('news-counter');
+    newsCounter.innerHTML = `
+    <h4 class="text-center bg-white p-4">${data.length} news found</h4>
+    `;
+
+
+    const newsContainer = document.getElementById('news-container');
+    newsContainer.innerHTML = ``;
+    // console.log(data);
     data.forEach(news =>{
         const newsId = news._id;
-        console.log(news);
+        // console.log(news);
         const newsDiv = document.createElement('div');
         // newsDiv.classList.add('card mb-3 container');
         newsDiv.classList.add('card', 'mb-4', 'fluid', 'border-0');
         
-        newsDiv.style.maxWidth = "840px";
+        newsDiv.style.maxWidth = "100%";
         newsDiv.style.maxHeight = '540px';
         newsDiv.innerHTML = `
-        <div onclick="newsDetails('${newsId}')" id="${newsId}" class="row g-0 p-4" data-bs-toggle="modal" data-bs-target="#newsModal">
+        <div onclick="newsDetails('${newsId}')" id="${newsId}" class="row g-0 p-4 mx-auto" data-bs-toggle="modal" data-bs-target="#newsModal">
         <div class="col-md-4">
           <img src="${news.thumbnail_url}" class="img-fluid" alt="">
         </div>
         <div class="col-md-8">
           <div class="card-body">
             <h5 class="card-title">${news.title}</h5>
-            <p style="overflow: hidden; text-overflow: ellipsis;" class="card-text text-secondary">${news.details.slice(0, 500)}</p>
+            <p style="
+            width: 100%; 
+            overflow: hidden;
+            text-overflow: ellipsis" class="card-text text-secondary">${news.details.slice(0, 500)}</p>
             <button onclick="newsDetails('${newsId}')" class='btn btn-primary' data-bs-toggle="modal" data-bs-target="#newsModal">Show News</button>
 
             <div class="d-flex">
@@ -76,12 +102,12 @@ const displayNews = (data) => {
                     <img width='40px' height='40px' src="${news.author.img}" style="border-radius: 50%;">
                 </div>
                 <div class="mt-3">
-                    <h5>${news.author.name}</h5>
+                    <h5>${news.author.name ? news.author.name : 'No author detected'}</h5>
                     <p class="text-secondary">${news.author.published_date}</p>
                 </div>
                 <div class="mx-5 mt-3">
                 <p>Total view</p>
-                <h6>${news.total_view}</h6>
+                <h6>${news ? news.total_view : 'No views'}</h6>
                 </div>
             </div>
 
@@ -94,10 +120,12 @@ const displayNews = (data) => {
         `;
         newsContainer.appendChild(newsDiv);
         
+        
+        
 
     })
 
-    document.getElementById('')
+    
 }
 
 // news details section start
@@ -106,15 +134,21 @@ const displayNews = (data) => {
 
 const newsDetails = async id => {
     // console.log(id);
-    const url = `https://openapi.programming-hero.com/api/news/${id}`;
+    try{
+        const url = `https://openapi.programming-hero.com/api/news/${id}`;
     const res = await fetch(url);
     const data = await res.json();
     displayNewsDetails(data.data[0]);
+    }
+    catch(error){
+        console.log(error);
+    }
 }
 
 const displayNewsDetails = details => {
     // console.log(details);
     const modalTitle = document.getElementById('newsModalLabel');
+    
     modalTitle.innerText = details.title;
 
     const modalDetails = document.getElementById('modal-detail');
